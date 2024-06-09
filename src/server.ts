ecd61cf826287bb9a3ab537a0cd2d09649502937
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-06-09 01:49:25 -0300

    fix: trying to fix deployment issue

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = fastify();
 
@@ -16,10 +19,17 @@ app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
 app.register(generateAiCompletionRoute);
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log("HTTP Server running!");
-  });
+// Ensure PORT is a string and provide a default value if not set
+const port = parseInt(process.env.PORT || '4000', 10);
+
+const start = async () => {
+  try {
+    await app.listen({ port, host: '0.0.0.0' });
+    app.log.info(`Server listening on port ${port}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
